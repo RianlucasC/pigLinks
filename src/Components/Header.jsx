93cd69afc84135e8a979cab73e76b782d982f6d7
Rianlucas7 <rianlucas592@gmail.com>
2023-07-29 01:51:40 -0300

    final

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <header className="px-6 py-1 flex gap-3 w-full border-b border-pinkLive items-center bg-blackLive">
-        <img src="../../public/porquinhoHeader.svg" alt="porquinho"/>
+        <img src="../../public/porquinho.png" alt="porquinho"/>
         <h1 className="text-2xl font-bold text-whiteText">piguinho</h1>
     </header>
   )
--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -4,31 +4,31 @@ const links = [
   {
     'id': 'Instagram',
     'text': 'Instagram',
-    'src': 'instagramSvg',
+    'src': 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg',
     'url': 'https://www.instagram.com/eupiguinho/',
   },
   {
     'id': 'Twitter',
     'text': 'Twitter',
-    'src': 'twitterSvg',
+    'src': 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg',
     'url': 'https://twitter.com/eupiguinho',
   },
   {
     'id': 'Youtube',
     'text': 'Piguinho',
-    'src': 'youtubeSvg',
+    'src': 'https://www.svgrepo.com/show/13671/youtube.svg',
     'url': 'https://www.youtube.com/piguinho1',
   },
   {
     'id': 'Tiktok',
     'text': 'Tiktok',
-    'src': 'tiktokSvg',
+    'src': 'https://seeklogo.com/images/T/tiktok-icon-logo-1CB398A1BD-seeklogo.com.png',
     'url': 'https://www.tiktok.com/@cortesdopig',
   },
   {
     'id': 'Discord',
     'text': 'Discord da live',
-    'src': 'discordSvg',
+    'src': 'https://www.svgrepo.com/show/353655/discord-icon.svg',
     'url': 'https://discord.com/invite/DU8HCXr5C4',
   },
   
